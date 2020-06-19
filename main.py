ef7a949fefcc88a9f3e8c99595f7cdb49ea6375f
SourLemon23 <57299559+SourLemon23@users.noreply.github.com>
2020-06-18 22:13:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tracker import track_stocks
+import tracker
 
 # - Tracks all stocks on the clipboard
 # - Sends email notifications if the current price changed by 5% or more from the buying price.
