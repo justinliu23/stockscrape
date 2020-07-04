6a0768582efe5b91568edec5482a9d8ca1805ef2
SourLemon23 <justinliu23@gmail.com>
2020-07-03 22:47:54 -0700

    Implemented CSV control files

--- tracker.py
+++ tracker.py
@@ -1,29 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import csv
 
-NAME = "name"
-BUY_PRICE = "buy price"
-# - Personal stocks clipboard in which you can edit
-# - Any changes will be reflected accordingly in the program
-# - Email notifications will be sent for the newly added stock
-stocks_clipboard = \
-    {
-        1:  {NAME: 'AAPL', BUY_PRICE: 331.76},
-        2:  {NAME: 'AMZN', BUY_PRICE: 2600.98},
-        3:  {NAME: 'BA'  , BUY_PRICE: 190.54},
-        4:  {NAME: 'FB'  , BUY_PRICE: 232.78},
-        5:  {NAME: 'GOOG', BUY_PRICE: 1435.96},
-        6:  {NAME: 'INTC', BUY_PRICE: 60.53},
-        7:  {NAME: 'MSFT', BUY_PRICE: 194.24},
-        8:  {NAME: 'NVDA', BUY_PRICE: 369.49},
-        9:  {NAME: 'TSLA', BUY_PRICE: 970.42},
-        10: {NAME: 'AMD' , BUY_PRICE: 55.21},
-        11: {NAME: 'UAL' , BUY_PRICE: 40.96},
-        12: {NAME: 'NUGT', BUY_PRICE: 65.38},
-        13: {NAME: 'XOM' , BUY_PRICE: 40.37},
-        14: {NAME: 'PLUG', BUY_PRICE: 5.51},
-    }
+fallen_price_factor = 0.95
+risen_price_factor = 1.05
+percentage_factor = 100
+
+stocks_clipboard_file = open('C:\\Users\\justi\\OneDrive\\Documents\\Software\\Python\\PycharmProjects\\Price Checkers\\Stocks\\stock-tracker\\Stocks Clipboard.csv', 'r')
+stocks_clipboard_reader = csv.reader(stocks_clipboard_file)
+
+emails_file = open('C:\\Users\\justi\\OneDrive\\Documents\\Software\\Python\\PycharmProjects\\Price Checkers\\Stocks\\stock-tracker\\Notifying Email Accounts.csv', 'r')
+emails_reader = csv.reader(emails_file)
+
+receiving_emails = []
+
+next(emails_reader)
+
+for row in emails_reader:
+    if row[0] != '':
+        sending_email = row[0]
+    if row[1] != '':
+        password = row[1]
+    if row[2] != '':
+        receiving_emails.append(row[2])
+    else:
+        break
+
+emails_file.close()
 
 
 # Returns the URL of a stock on Yahoo! Finance based on any stock symbol
@@ -39,38 +43,30 @@ def get_current_stock_price(stock_url):
     page.raise_for_status()
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    current_stock_price = soup.find('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
+    current_stock_price_tag = soup.find('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span')
+    current_stock_price = current_stock_price_tag.text.replace(',', '')
     return current_stock_price
 
 
-# Checks if any stock on the clipboard fell below 5% of the buying price
-def check_price_fall():
-    fallen_price_factor = 0.95
+# Checks if any stock on Stocks Clipboard.csv fell or rose over 5% of the buying price
+def check_price_change():
+    next(stocks_clipboard_reader)
 
-    for i in range(len(stocks_clipboard)):
-        stock = stocks_clipboard[i + 1].get(NAME)
+    for row in stocks_clipboard_reader:
+        stock = row[0]
         stock_url = get_stock_url(stock)
-        stock_buy_price = float(str(stocks_clipboard[i + 1].get(BUY_PRICE)).replace(',', ''))
-        current_stock_price = float(get_current_stock_price(stock_url).replace(',', ''))
+        stock_buy_price = float(str(row[1].replace(',', '')))
+        current_stock_price = float(get_current_stock_price(stock_url))
 
         if current_stock_price < (fallen_price_factor * stock_buy_price):
-            send_fallen_price_email(stock, stock_buy_price)
+            send_email_notification(stock, stock_buy_price, 'fallen')
             print('Email sent: ' + stock + ' fell below 5% of your buying price.')
 
-
-# Checks if any stock on the clipboard rose above 5% of the buying price
-def check_price_rise():
-    risen_price_factor = 1.05
-
-    for i in range(len(stocks_clipboard)):
-        stock = stocks_clipboard[i + 1].get(NAME)
-        stock_url = get_stock_url(stock)
-        stock_buy_price = float(str(stocks_clipboard[i + 1].get(BUY_PRICE)).replace(',', ''))
-        current_stock_price = float(get_current_stock_price(stock_url).replace(',', ''))
-
-        if current_stock_price > (risen_price_factor * stock_buy_price):
-            send_risen_price_email(stock, stock_buy_price)
+        elif current_stock_price > (risen_price_factor * stock_buy_price):
+            send_email_notification(stock, stock_buy_price, 'risen')
             print('Email sent: ' + stock + ' rose above 5% of your buying price.')
+
+    stocks_clipboard_file.close()
 
 
 # Sends an email to a specified email address if any stock on the clipboard fell below 5% of the buying price
@@ -78,79 +74,54 @@ def check_price_rise():
 # - Requires Google 2-Step Verification to be enabled
 # - The password is a generated Google App Password for one's Gmail account
 # --------------------------------------------------------------------------
-def send_fallen_price_email(stock, stock_buy_price):
+def send_email_notification(stock, stock_buy_price, email_type):
     stock_url = get_stock_url(stock)
     stock_current_price = float(get_current_stock_price(stock_url))
-
-    percentage_factor = 100
-
-    stock_fallen_amount = percentage_factor * ((stock_buy_price - stock_current_price) / stock_buy_price)
-    sliced_stock_fallen_amount = str(stock_fallen_amount)[:4]
-    stock_price_fallen_percentage = -float(sliced_stock_fallen_amount)
 
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
     smtp.ehlo()
     smtp.starttls()
 
-    sending_email = 'someone@example.com'
-    receiving_emails = ['someone@example.com', 'someone@example.com', 'someone@example.com']
-    password = 'Insert your generated Gmail App Password here'
     smtp.login(sending_email, password)
 
-    subject = 'Stock Price Fall: ' + stock
-    body = stock + ' fell ' + str(stock_price_fallen_percentage) + '%' + ' below your buying price.' + '\n\n' + \
-           '-------------------------------------' + '\n' + \
-           'Buying price: ' + ' $' + str(stock_buy_price) + '\n' + \
-           'Current price: ' + '$' + str(stock_current_price) + '\n' + \
-           '-------------------------------------' + '\n\n' + \
-           'View the stock: ' + '\n' + stock_url
+    if email_type == 'fallen':
+        stock_fallen_amount = percentage_factor * ((stock_buy_price - stock_current_price) / stock_buy_price)
+        sliced_stock_fallen_amount = str(stock_fallen_amount)[:4]
+        stock_price_fallen_percentage = -float(sliced_stock_fallen_amount)
 
-    message = f'Subject: {subject}\n{body}'
+        subject = 'Stock Price Fall: ' + stock
+        body = stock + ' fell ' + str(stock_price_fallen_percentage) + '%' + ' below your buying price.' + '\n\n' + \
+               '-------------------------------------' + '\n' + \
+               'Buying price: ' + ' $' + str(stock_buy_price) + '\n' + \
+               'Current price: ' + '$' + str(stock_current_price) + '\n' + \
+               '-------------------------------------' + '\n\n' + \
+               'View the stock: ' + '\n' + stock_url
 
-    smtp.sendmail(sending_email, receiving_emails, message)
-    smtp.quit()
+        message = f'Subject: {subject}\n{body}'
 
+        smtp.sendmail(sending_email, receiving_emails, message)
+        smtp.quit()
 
-# Sends an email to specified email addresses if any stock on the clipboard rose above 5% of the buying price
-# --------------------------------------------------------------------------
-# - Requires Google 2-Step Verification to be enabled
-# - The password is a generated Google App Password for one's Gmail account
-# --------------------------------------------------------------------------
-def send_risen_price_email(stock, stock_buy_price):
-    stock_url = get_stock_url(stock)
-    stock_current_price = float(get_current_stock_price(stock_url))
+    elif email_type == 'risen':
+        stock_risen_amount = percentage_factor * ((stock_current_price - stock_buy_price) / stock_buy_price)
+        stock_sliced_risen_amount = str(stock_risen_amount)[:4]
+        stock_price_rise_percentage = float(stock_sliced_risen_amount)
 
-    percentage_factor = 100
+        subject = 'Stock Price Rise: ' + stock
+        body = stock + ' rose ' + str(stock_price_rise_percentage) + '%' + ' above your buying price.' + '\n\n' + \
+               '-------------------------------------' + '\n' + \
+               'Buying price: ' + ' $' + str(stock_buy_price) + '\n' + \
+               'Current price: ' + '$' + str(stock_current_price) + '\n' + \
+               '-------------------------------------' + '\n\n' + \
+               'View the stock: ' + '\n' + stock_url
 
-    stock_risen_amount = percentage_factor * ((stock_current_price - stock_buy_price) / stock_buy_price)
-    stock_sliced_risen_amount = str(stock_risen_amount)[:4]
-    stock_price_rise_percentage = float(stock_sliced_risen_amount)
+        message = f'Subject: {subject}\n{body}'
 
-    smtp = smtplib.SMTP('smtp.gmail.com', 587)
-    smtp.ehlo()
-    smtp.starttls()
-
-    sending_email = 'someone@example.com'
-    receiving_emails = ['someone@example.com', 'someone@example.com', 'someone@example.com']
-    password = 'Insert your generated Gmail App Password here'
-    smtp.login(sending_email, password)
-
-    subject = 'Stock Price Rise: ' + stock
-    body = stock + ' rose ' + str(stock_price_rise_percentage) + '%' + ' above your buying price.' + '\n\n' + \
-           '-------------------------------------' + '\n' + \
-           'Buying price: ' + ' $' + str(stock_buy_price) + '\n' + \
-           'Current price: ' + '$' + str(stock_current_price) + '\n' + \
-           '-------------------------------------' + '\n\n' + \
-           'View the stock: ' + '\n' + stock_url
-
-    message = f'Subject: {subject}\n{body}'
-
-    smtp.sendmail(sending_email, receiving_emails, message)
-    smtp.quit()
+        smtp.sendmail(sending_email, receiving_emails, message)
+        smtp.quit()
 
 
-# - Tracks all stocks on the clipboard
-# - Sends email notifications if the current price changed by 5% or more from the buying price
+# - Tracks stocks based on a CSV file defined by the user
+# - Sends email notifications if a stock's current price changed by 5% or more from the buying price
 def track_stocks():
-    check_price_fall()
-    check_price_rise()
+    check_price_change()
